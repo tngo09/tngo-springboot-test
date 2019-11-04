6174861303174130d1a21047284081b22d6b9b11
tngo09 <38280434+tngo09@users.noreply.github.com>
2019-11-04 15:40:15 +0100

    Add files via upload

---
+++ src/main/java/com/tng/springboot/sample1/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.tng.springboot.sample1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author A292423
+ *
+ */
+@SpringBootApplication
+public class HelloWorld {
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorld.class, args);
+	}
+
+}
